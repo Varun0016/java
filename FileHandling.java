3f9ceff69240bf5904fddc540c3bd86bc0df9cf5
Varun0016 <157778391+Varun0016@users.noreply.github.com>
2024-03-27 11:27:19 +0530

    Add files via upload

---
+++ FileHandling.java
@@ -0,0 +1,29 @@
+package FirstPackage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class FileHandling {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("info.text");
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter writer = new FileWriter(file);
+		writer.write("one");
+		writer.append("two");
+		writer.write("three");
+		writer.close();
+		
+		File readFile = new File("data.txt");
+		Scanner sc = new Scanner(readFile);
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+
+	}
+
+}
