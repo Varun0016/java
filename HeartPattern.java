9a081147f51e2543fc0ba5f4cdd27273d35b87e3
Varun D <157778391+Varun0016@users.noreply.github.com>
2024-08-10 11:03:36 +0530

    Add files via upload

--- HeartPattern.java
+++ HeartPattern.java
@@ -1,35 +1,29 @@
+package practice;
+
 public class HeartPattern {
     public static void main(String[] args) {
-        int size = 6; // Adjust the size of the heart shape here
-        drawUpperHeart(size);
-        drawLowerHeart(size);
-    }
-
-    public static void drawUpperHeart(int size) {
-        for (int i = size / 2; i <= size; i += 2) {
-            for (int j = 1; j < size - i; j += 2) {
+        for(int i=1;i<=5;i++) {
+            for(int j=5;j>=i;j--) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for(int k=1;k<=i;k++){
+                System.out.print(" *");
             }
-            for (int j = 1; j <= size - i; j++) {
-                System.out.print(" ");
+            for(int s=4;s>=i;s--){
+                System.out.print("  ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for(int m=1;m<=i;m++) {
+                System.out.print(" *");
             }
             System.out.println();
         }
-    }
-
-    public static void drawLowerHeart(int size) {
-        for (int i = size; i >= 1; i--) {
-            for (int j = i; j < size; j++) {
+        
+        for(int x=1;x<=10;x++) {
+            for(int y=1;y<=x;y++){
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (i * 2) - 1; j++) {
-                System.out.print("*");
+            for(int z=10;z>=x;z--){
+                System.out.print(" *");
             }
             System.out.println();
         }
